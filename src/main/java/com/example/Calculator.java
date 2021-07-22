f852e1b38e57c27e1c358eaf25770bf470855f81
Bcorral <beatrizcorralcampos@gmail.com>
2021-07-22 10:57:46 +0200

    crear proyecto

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,21 @@
+package com.example;
+/**
+ * Clase para cálculos artimeticos
+ */
+public class Calculator {
+	/**
+	 * Metodo para suma
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	
+	
+	public int sum(int num1, int num2) {
+		int abc;
+		return num1 + num2;
+}
+	public int mul(int num1, int num2) {
+		return num1 + num2;
+}
+}
